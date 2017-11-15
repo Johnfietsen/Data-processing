c5959c98a38dd8f6f0f1c3c7cbb2acf84b93690a
Johnfietsen <lucstefelmanns@live.nl>
2017-11-15 15:52:31 +0100

    Kijken of dit werkt.

--- Week 3/convertCSV2JSON.py
+++ Week 3/convertCSV2JSON.py
@@ -3,3 +3,13 @@
 # Author:   L.K. Stefelmanns
 # Course:   Data processing
 # Study:    Minor Programming, University of Amsterdam
+
+import csv
+
+
+csv_input = open('sealevel_data.txt', 'r')
+print(csv_input)
+
+# def convert_CSV_to_JSON(csv_input):
+#
+#     json_output = {}
