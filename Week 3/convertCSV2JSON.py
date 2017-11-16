972a9592797dd553a1c70b5a58e3e9ad76daa173
Johnfietsen <lucstefelmanns@live.nl>
2017-11-16 09:15:17 +0100

    CVS2JSON bijna klaar.

--- Week 3/convertCSV2JSON.py
+++ Week 3/convertCSV2JSON.py
@@ -4,12 +4,35 @@
 # Course:   Data processing
 # Study:    Minor Programming, University of Amsterdam
 
+'''
+Function that takes CSV data (saved in a txt file) and converts it into JSON
+data.
+'''
+
+
 import csv
 
 
-csv_input = open('sealevel_data.txt', 'r')
-print(csv_input)
+test_input = open('C:/Users/lucst/Desktop/Minor programmeren/GitHub/data_processing/Week 3/sealevel_data.txt', 'r')
+test_output = open('C:/Users/lucst/Desktop/Minor programmeren/GitHub/data_processing/Week 3/sealevel_data.json', 'w')
 
-# def convert_CSV_to_JSON(csv_input):
-#
-#     json_output = {}
+def convert_CSV_to_JSON(csv_input):
+
+    tmp_list = []
+    JSON_list = []
+
+    for line in csv_input:
+        tmp_list.append(line.split(','))
+
+    title_one = str(tmp_list[0][0])
+    title_two = str(tmp_list[0][1])
+
+    for i in range(1, len(tmp_list) - 1):
+        JSON_list.append({title_one : tmp_list[i][0],
+                          title_two : tmp_list[i][1]})
+
+    return JSON_list
+
+test_JSON = convert_CSV_to_JSON(test_input)
+
+test_output = wr
