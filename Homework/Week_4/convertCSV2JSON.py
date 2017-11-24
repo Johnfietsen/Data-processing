e2c96084eae4cb959b2a5dcccad4a87899352cca
Johnfietsen <lucstefelmanns@live.nl>
2017-11-24 12:26:55 +0100

    Opdracht klaar, nog alleen comments.

--- Homework/Week_4/convertCSV2JSON.py
+++ Homework/Week_4/convertCSV2JSON.py
@@ -1,8 +1,10 @@
+#
 # convertCSV2JSON.py
 #
 # Author:   L.K. Stefelmanns
 # Course:   Data processing
 # Study:    Minor Programming, University of Amsterdam
+#
 
 '''
 Function that takes CSV data (saved in a txt file) and converts it into JSON
@@ -45,13 +47,14 @@ def convert_CSV_to_JSON(csv_input1, csv_input2, csv_input3, csv_input4):
         if (tmp_list1[i][0].strip('\"') is not '' and
            tmp_list1[i][58].strip('\"') is not '' and
            tmp_list3[i][58].strip('\"') is not '' and
+           float(tmp_list3[i][58].strip('\"')) > 200000000000 and
            tmp_list4[i][2].strip('\"') is not ''):
 
-            JSON_list.append({'country_name' : tmp_list1[i][0].strip('\"'),
-                              'COtwo' : tmp_list1[i][58].strip('\"'),
-                              'GDP_growth' : tmp_list2[i][58].strip('\"'),
-                              'GDP_total' : tmp_list3[i][58].strip('\"'),
-                              'income_category' : tmp_list4[i][2].strip('\"')})
+           JSON_list.append({'country_name' : tmp_list1[i][0].strip('\"'),
+                             'COtwo' : tmp_list1[i][58].strip('\"'),
+                             'GDP_growth' : tmp_list2[i][58].strip('\"'),
+                             'GDP_total' : tmp_list3[i][58].strip('\"'),
+                             'income_category' : tmp_list4[i][2].strip('\"')})
 
 
     JSON_data = json.loads(json.dumps(JSON_list))
--- Homework/Week_4/scatterplot.js
+++ Homework/Week_4/scatterplot.js
@@ -1,104 +1,120 @@
-////////////////////////////////////////////////////////////////////////////////
+//
 //  scatterplot.js
 //
 //  Author:   L.K. Stefelmanns
 //  Course:   Data processing
 //  Study:    Minor Programming, University of Amsterdam
-////////////////////////////////////////////////////////////////////////////////
+//
+// Javascript imported by scatterplot.html to create the scatterplot based on
+// data from scatterplot.json.
+//
 
+window.onload = function(){
 
-var margin = {top: 20, right: 20, bottom: 30, left: 60},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var margin = {top: 50, right: 20, bottom: 30, left: 60},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([0, width]);
+    var x = d3.scale.linear()
+        .range([0, width]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-var z = d3.scale.linear()
-    .range([0, 10]);
+    var z = d3.scale.linear()
+        .range([3, 40]);
 
-var color = d3.scale.category10();
+    var color = d3.scale.ordinal()
+        .range(["#005824", "#238b45", "#66c2a4", "#ccece6"])
+        .domain(["High income", "Upper middle income",
+                 "Lower middle income", "Low income"]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("scatterplot.json", function(error, data) {
-  if (error) throw error;
+    d3.json("scatterplot.json", function(error, data) {
+      if (error) throw error;
 
-  data.forEach(function(d) {
-    d.COtwo = +d.COtwo;
-    d.GDP_growth = +d.GDP_growth;
-  });
+      data.forEach(function(d) {
+        d.COtwo = +d.COtwo;
+        d.GDP_growth = +d.GDP_growth
+        d.GDP_total = +d.GDP_total;
+      });
 
-  x.domain(d3.extent(data, function(d) { return d.GDP_growth; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.COtwo; })).nice();
-  // z.domain(d3.extent(data, function(d) { return d.GDP_total; })).nice();
+      x.domain(d3.extent(data, function(d) { return d.GDP_growth; })).nice();
+      y.domain(d3.extent(data, function(d) { return d.COtwo; })).nice();
+      z.domain(d3.extent(data, function(d) { return d.GDP_total; })).nice();
 
+      svg.append("text")
+              .attr("x", 0)
+              .attr("y", 0 - (margin.top / 2))
+              .attr("text-anchor", "left")
+              .style("font-size", "16px")
+              .text("GDP growth compared to annual CO2 emmissions (2014)");
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("GDP growth (perc)");
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+        .append("text")
+          .attr("class", "label")
+          .attr("x", width)
+          .attr("y", -6)
+          .style("text-anchor", "end")
+          .text("GDP growth (perc)");
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("CO2 (tn)")
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("class", "label")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("CO2 (tn)")
 
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)//function(d) { return z(d.GDP_total); })
-      .attr("cx", function(d) { return x(d.GDP_growth); })
-      .attr("cy", function(d) { return y(d.COtwo); })
-      .style("fill", function(d) { return color(d.income_category); });
+      svg.selectAll(".dot")
+          .data(data)
+        .enter().append("circle")
+          .attr("class", "dot")
+          .attr("r", function(d) { return z(d.GDP_total); })
+          .attr("cx", function(d) { return x(d.GDP_growth); })
+          .attr("cy", function(d) { return y(d.COtwo); })
+          .style("fill", function(d) { return color(d.income_category); });
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) {
-                                         return "translate(0," + i * 20 + ")";
-                                        });
+      var legend = svg.selectAll(".legend")
+          .data(color.domain())
+        .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) {
+                                             return "translate(0," + i * 20 + ")";
+                                            });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+      legend.append("rect")
+          .attr("x", width - 18)
+          .attr("width", 18)
+          .attr("height", 18)
+          .style("fill", color);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
+      legend.append("text")
+          .attr("x", width - 24)
+          .attr("y", 9)
+          .attr("dy", ".35em")
+          .style("text-anchor", "end")
+          .text(function(d) { return d; });
 
-});
+    });
+
+}
